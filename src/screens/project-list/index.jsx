75b29df4b53f2cf4a4ffc9a6a84ca3738b0adeac
usamihal <usamihal001@163.com>
2021-10-29 15:04:05 +0800

    feat: 添加 useMount 和 useDebounce 两个hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react'
 import { SearchPanel } from './search-panel'
 import { List } from './list'
-import { cleanObject } from 'utils'
+import { cleanObject, useDebounce, useMount } from 'utils'
 import * as qs from 'qs'
 
 const apiUrl = process.env.REACT_APP_API_URL
@@ -12,25 +12,27 @@ export const ProjectListScreen = () => {
     name: '',
     personId: ''
   })
+
+  const debouncedParam = useDebounce(param, 2000)
   const [list, setList] = useState([])
 
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(
-      async response => {
-        if (response.ok) {
-          setList(await response.json())
-        }
+    fetch(
+      `${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`
+    ).then(async response => {
+      if (response.ok) {
+        setList(await response.json())
       }
-    )
-  }, [param])
+    })
+  }, [debouncedParam])
 
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
   return (
     <div>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,5 @@
+import { useEffect, useState } from 'react'
+
 export const isFalsy = value => (value === 0 ? false : !value)
 export const cleanObject = object => {
   const result = { ...object }
@@ -8,4 +10,23 @@ export const cleanObject = object => {
     }
   })
   return result
+}
+
+export const useMount = callback => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value, delay) => {
+  // 用useState 定义的状态，会触发组件刷新
+  const [debouncedValue, setDebouncedValue] = useState(value)
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+    // 对于useEffect，useEffect内部运行之前，会执行上一次useEffect返回的回调函数
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+  return debouncedValue
 }
