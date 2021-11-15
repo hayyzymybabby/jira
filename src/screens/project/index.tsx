b26ce54ff08b31fec2ba86db12f7ae90e91cdfad
usamihal <usamihal001@163.com>
2021-11-15 22:51:28 +0800

    feat: kanban和epic路由添加

--- src/screens/project/index.tsx
+++ src/screens/project/index.tsx
@@ -1,3 +1,20 @@
+import { Link } from 'react-router-dom'
+import { Routes, Route } from 'react-router'
+import { KanbanScreen } from 'screens/Kanban'
+import { EpicScreen } from 'screens/Epic'
 export const ProjectScreen = () => {
-  return <h1>ProjectScreen</h1>
+  return (
+    <div>
+      <h1>ProjectScreen</h1>
+      <Link to={'kanban'}>看板</Link>
+      <Link to={'epic'}>任务组</Link>
+      <Routes>
+        {/*projects/:projectId/kanban*/}
+        <Route path={'kanban'} element={<KanbanScreen />} />
+        {/*projects/:projectId/epic*/}
+        <Route path={'epic'} element={<EpicScreen />} />
+        <Route index element={<KanbanScreen />} />
+      </Routes>
+    </div>
+  )
 }
