59e441cb36f785c1747d5d4a14370eff3b67d135
usamihal <usamihal001@163.com>
2021-11-17 23:15:06 +0800

    feat: 用useEditProject 编辑项目

--- src/screens/project-list/list.tsx
+++ src/screens/project-list/list.tsx
@@ -1,6 +1,8 @@
 import { Table, TableProps } from 'antd'
+import { Pin } from 'components/pin'
 import dayjs from 'dayjs'
 import { Link } from 'react-router-dom'
+import { useEditProject } from 'utils/project'
 import { User } from './search-panel'
 
 export interface Project {
@@ -17,11 +19,24 @@ interface ListProps extends TableProps<Project> {
 }
 
 export const List = ({ users, ...props }: ListProps) => {
+  const { mutate } = useEditProject()
+  const pinProject = (id: number) => (pin: boolean) => mutate({ id, pin })
   return (
     <Table
       rowKey={'id'}
       pagination={false}
       columns={[
+        {
+          title: <Pin checked={true} disabled={true} />,
+          render(value, project) {
+            return (
+              <Pin
+                checked={project.pin}
+                onCheckedChange={pinProject(project.id)}
+              />
+            )
+          }
+        },
         {
           title: '名称',
           sorter: (a, b) => a.name.localeCompare(b.name),
