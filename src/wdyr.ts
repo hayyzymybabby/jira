3eb83e29b3ce232036c9fe2207ad268195f96072
usamihal <usamihal001@163.com>
2021-11-17 22:16:22 +0800

    feat: 用user-select.tsx选择用户

--- src/wdyr.ts
+++ src/wdyr.ts
@@ -3,6 +3,6 @@ import React from 'react'
 if (process.env.NODE_ENV === 'development') {
   const whyDidyouRender = require('@welldone-software/why-did-you-render')
   whyDidyouRender(React, {
-    trackAllPureComponents: true
+    trackAllPureComponents: false
   })
 }
