8dd21e2c73235b2fa68672ac7b3e563dbf5701ea
usamihal <usamihal001@163.com>
2021-11-23 21:21:33 +0800

    feat: 用状态提升实现projectModal状态管理

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -2,12 +2,14 @@ import styled from '@emotion/styled'
 import { SearchPanel } from './search-panel'
 import { List } from './list'
 import { useDebounce, useDocumentTitle } from 'utils'
-import { Typography } from 'antd'
+import { Button, Row, Typography } from 'antd'
 import { useProjects } from 'utils/project'
 import { useUsers } from 'utils/user'
 import { useProjectsSearchParams } from './util'
 
-export const ProjectListScreen = () => {
+export const ProjectListScreen = (props: {
+  setProjectModalOpen: (isOpen: boolean) => void
+}) => {
   useDocumentTitle('项目列表', false)
 
   const [param, setParam] = useProjectsSearchParams()
@@ -21,7 +23,12 @@ export const ProjectListScreen = () => {
 
   return (
     <Container>
-      <h1>项目列表</h1>
+      <Row justify={'space-between'}>
+        <h1>项目列表</h1>
+        <Button onClick={() => props.setProjectModalOpen(true)}>
+          创建项目
+        </Button>
+      </Row>
       <SearchPanel
         users={users || []}
         param={param}
@@ -31,6 +38,7 @@ export const ProjectListScreen = () => {
         <Typography.Text type={'danger'}>{error.message}</Typography.Text>
       ) : null}
       <List
+        setProjectModalOpen={props.setProjectModalOpen}
         refresh={retry}
         loading={isLoading}
         users={users || []}
