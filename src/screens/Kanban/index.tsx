62f21feaed9317e801ddcfa3e98512a35e31b281
usamihal <usamihal001@163.com>
2021-12-30 13:18:05 +0800

    feat: 实现拖拽

--- src/screens/Kanban/index.tsx
+++ src/screens/Kanban/index.tsx
@@ -1,6 +1,8 @@
 import styled from '@emotion/styled'
 import { Spin } from 'antd'
+import { Drag, Drop, DropChild } from 'components/drag-and-drop'
 import { ScreenContainer } from 'components/lib'
+import { DragDropContext } from 'react-beautiful-dnd'
 import { useDocumentTitle } from 'utils'
 import { useKanbans } from 'utils/kanban'
 import { useTasks } from 'utils/task'
@@ -25,21 +27,40 @@ export const KanbanScreen = () => {
   const isLoading = taskIsLoading || kanbanIsLoading
 
   return (
-    <ScreenContainer>
-      <h1>{currentProject?.name}看板</h1>
-      <SearchPanel />
-      {isLoading ? (
-        <Spin size={'large'} />
-      ) : (
-        <ColumnsContainer>
-          {kanbans?.map(kanban => (
-            <KanbanColumn kanban={kanban} key={kanban.id}></KanbanColumn>
-          ))}
-          <CreateKanban />
-        </ColumnsContainer>
-      )}
-      <TaskModal />
-    </ScreenContainer>
+    <DragDropContext onDragEnd={() => {}}>
+      <ScreenContainer>
+        <h1>{currentProject?.name}看板</h1>
+        <SearchPanel />
+        {isLoading ? (
+          <Spin size={'large'} />
+        ) : (
+          <ColumnsContainer>
+            <Drop
+              type={'COLUMN'}
+              direction={'horizontal'}
+              droppableId={'kanban'}
+            >
+              <DropChild style={{ display: 'flex' }}>
+                {kanbans?.map((kanban, index) => (
+                  <Drag
+                    key={kanban.id}
+                    draggableId={'kanban' + kanban.id}
+                    index={index}
+                  >
+                    <KanbanColumn
+                      kanban={kanban}
+                      key={kanban.id}
+                    ></KanbanColumn>
+                  </Drag>
+                ))}
+              </DropChild>
+            </Drop>
+            <CreateKanban />
+          </ColumnsContainer>
+        )}
+        <TaskModal />
+      </ScreenContainer>
+    </DragDropContext>
   )
 }
 
