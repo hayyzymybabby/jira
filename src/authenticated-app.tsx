1e36c46dc9d2d256e02afa9c5035f0a8e5fa4490
usamihal <usamihal001@163.com>
2024-01-09 09:53:37 +0800

    feat: 用CSS-in-JS状态工程创建自定义组件：Row组件实现

--- src/authenticated-app.tsx
+++ src/authenticated-app.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled'
+import { Row } from 'components/lib'
 import { useAuth } from 'context/auth-context'
 import { ProjectListScreen } from 'screens/project-list'
 
@@ -6,8 +7,8 @@ export const AuthenticatedApp = () => {
   const { logout } = useAuth()
   return (
     <Container>
-      <Header>
-        <HeaderLeft>
+      <Header between={true}>
+        <HeaderLeft gap={true}>
           <h3>Logo</h3>
           <h3>项目</h3>
           <h3>用户</h3>
@@ -16,12 +17,9 @@ export const AuthenticatedApp = () => {
           <button onClick={logout}>登出</button>
         </HeaderRight>
       </Header>
-      <Nav>nav</Nav>
       <Main>
         <ProjectListScreen />
       </Main>
-      <Aside>Aside</Aside>
-      <Footer>Footer</Footer>
     </Container>
   )
 }
@@ -39,38 +37,15 @@ export const AuthenticatedApp = () => {
  */
 
 const Container = styled.div`
-  display: grid;
-  grid-template-rows: 6rem 1fr 6rem;
-  grid-template-columns: 20rem 1fr 20rem;
-  grid-template-areas:
-    'header header header'
-    'nav main aside'
-    'footer footer footer';
   height: 100vh;
-  grid-gap: 10rem;
 `
 // grid-area 用来给grid子元素起名字
-const Header = styled.header`
-  grid-area: header;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: space-between;
-`
-const HeaderLeft = styled.div`
+const Header = styled(Row)``
+const HeaderLeft = styled(Row)`
   display: flex;
   align-items: center;
 `
 const HeaderRight = styled.div``
 const Main = styled.main`
   grid-area: main;
-`
-const Nav = styled.nav`
-  grid-area: nav;
-`
-const Aside = styled.aside`
-  grid-area: aside;
-`
-const Footer = styled.footer`
-  grid-area: footer;
 `
