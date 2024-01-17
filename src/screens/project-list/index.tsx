df088c8a80acc2c34f513fc1c186802a907d6963
usamihal <usamihal001@163.com>
2024-01-17 11:18:36 +0800

    feat: 用高级 Hook-useAsync统一处理Loading和Error状态

--- src/screens/project-list/index.tsx
+++ src/screens/project-list/index.tsx
@@ -1,52 +1,30 @@
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
 import styled from '@emotion/styled'
 import { SearchPanel } from './search-panel'
 import { List } from './list'
-import { cleanObject, useDebounce, useMount } from '../../utils'
-import { useHttp } from 'utils/http'
+import { useDebounce } from '../../utils'
 import { Typography } from 'antd'
+import { useProjects } from 'utils/project'
+import { useUsers } from 'utils/user'
 
 export const ProjectListScreen = () => {
-  const [users, setUsers] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
-  const [error, setError] = useState<null | Error>(null)
-
   const [param, setParam] = useState({
     name: '',
     personId: ''
   })
 
   const debouncedParam = useDebounce(param, 300)
-
-  const [list, setList] = useState([])
-  const client = useHttp()
-
-  useEffect(() => {
-    setIsLoading(true)
-    client('projects', { data: cleanObject(debouncedParam) })
-      .then(setList)
-      .catch(error => {
-        setList([])
-        setError(error)
-      })
-      .finally(() => {
-        setIsLoading(false)
-      })
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [debouncedParam])
-
-  useMount(() => {
-    client('users').then(setUsers)
-  })
+  const { isLoading, error, data: list } = useProjects(debouncedParam)
+  const { data: users } = useUsers()
 
   return (
     <Container>
       <h1>项目列表</h1>
-      <SearchPanel users={users} param={param} setParam={setParam} />
+      <SearchPanel users={users || []} param={param} setParam={setParam} />
       {error ? (
         <Typography.Text type={'danger'}>{error.message}</Typography.Text>
       ) : null}
-      <List loading={isLoading} users={users} dataSource={list} />
+      <List loading={isLoading} users={users || []} dataSource={list || []} />
     </Container>
   )
 }
