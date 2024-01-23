6e8bf8d418415546ca17a76108cfaa7ad71bde7d
usamihal <usamihal001@163.com>
2024-01-23 14:49:05 +0800

    feat: 用useAsync获取用户信息

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -1,8 +1,10 @@
-import React, { ReactNode, useState } from 'react'
+import React, { ReactNode } from 'react'
 import * as auth from 'auth-provider'
 import { User } from 'screens/project-list/search-panel'
 import { http } from 'utils/http'
 import { useMount } from 'utils'
+import { useAsync } from 'utils/use-async'
+import { FullPageErrorFallback, FullPageLoading } from 'components/lib'
 
 interface AuthForm {
   username: string
@@ -33,7 +35,15 @@ const AuthContext = React.createContext<
 AuthContext.displayName = 'AuthContext'
 
 export const AuthProvider = ({ children }: { children: ReactNode }) => {
-  const [user, setUser] = useState<User | null>(null)
+  const {
+    data: user,
+    error,
+    isLoading,
+    isIdle,
+    isError,
+    run,
+    setData: setUser
+  } = useAsync<User | null>()
 
   // user => setUser(user) 可以简写成 setUser point free
   const login = (form: AuthForm) => auth.login(form).then(setUser)
@@ -44,8 +54,16 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   const logout = () => auth.logout().then(() => setUser(null))
 
   useMount(() => {
-    bootstrapUser().then(setUser)
+    run(bootstrapUser())
   })
+
+  if (isIdle || isLoading) {
+    return <FullPageLoading />
+  }
+
+  if (isError) {
+    return <FullPageErrorFallback error={error} />
+  }
 
   return (
     <AuthContext.Provider
