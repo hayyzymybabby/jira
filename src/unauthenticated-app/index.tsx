2ba6d0c86bb4e65759995a941b4825122ddbe0bd
usamihal <usamihal001@163.com>
2024-02-28 09:52:57 +0800

    feat: 类型守卫，用useQuery缓存工程列表

--- src/unauthenticated-app/index.tsx
+++ src/unauthenticated-app/index.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react'
 import { RegisterScreen } from './register'
 import { LoginScreen } from './login'
-import { Card, Divider, Button, Typography } from 'antd'
+import { Card, Divider, Button } from 'antd'
 import styled from '@emotion/styled'
 import logo from 'assets/logo.svg'
 import left from 'assets/left.svg'
 import right from 'assets/right.svg'
 import { useDocumentTitle } from 'utils'
+import { ErrorBox } from 'components/lib'
 // import { Helmet } from 'react-helmet'
 
 export const UnauthenticatedApp = () => {
@@ -29,9 +30,7 @@ export const UnauthenticatedApp = () => {
       <Background />
       <ShadowCard>
         <Title>{isRegister ? '请注册' : '请登录'}</Title>
-        {error ? (
-          <Typography.Text type={'danger'}>{error.message}</Typography.Text>
-        ) : null}
+        <ErrorBox error={error} />
         {isRegister ? (
           <RegisterScreen onError={setError} />
         ) : (
