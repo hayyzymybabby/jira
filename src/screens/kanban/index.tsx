65466afbb8ee7790106c1c5f2ce02f0176dad03c
usamihal <usamihal001@163.com>
2024-03-07 23:00:31 +0800

    feat: 添加任务搜索功能

--- src/screens/kanban/index.tsx
+++ src/screens/kanban/index.tsx
@@ -3,6 +3,7 @@ import { useKanbans } from 'utils/kanban'
 import { useKanbanSearchParams, useProjectInUrl } from './util'
 import { KanbanColumn } from './kanban-column'
 import styled from '@emotion/styled'
+import { SearchPanel } from './search-panel'
 
 export const KanbanScreen = () => {
   useDocumentTitle('看板列表')
@@ -12,6 +13,7 @@ export const KanbanScreen = () => {
   return (
     <div>
       <h1>{currentProject?.name}看板</h1>
+      <SearchPanel />
       <ColumnsContainer>
         {kanbans?.map(kanban => (
           <KanbanColumn kanban={kanban} key={kanban.id} />
